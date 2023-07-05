101a3a39bb4461711a86c6b9d73d75109ec2e742
eno-conan <eno49study@gmail.com>
2023-07-06 07:34:27 +0900

    switch right darkの微修正

--- switch_right_dark.py
+++ switch_right_dark.py
@@ -5,9 +5,6 @@ from flet import (
     Page,
     ButtonStyle,
     ProgressBar,
-    AppBar,
-    Text,
-    icons,
 )
 
 from time import sleep
@@ -24,37 +21,36 @@ class ToggleDarkLight():
         super().__init__(*args, **kwargs)
         self.page = page
 
-        # ADD PROGESSBAR EFFECT WHEN CHANGE LIGHT OR DARK
+        # 切り替え時にプログレスバー表示
         page.splash = ProgressBar(visible=False)
 
         def change_theme(e):
-            # page.splash.visible = True
+            # プログレスバー表示
+            page.splash.visible = True
             page.theme_mode = "light" if page.theme_mode == "dark" else "dark"
             page.update()
 
-            # DELAY EFFECT THE ANIMATION
-            sleep(0.5)
+            # アニメーション適用のための時間確保(1s)
+            sleep(1)
 
-            # CHANGE THE ICON DARK MODE OR LIGHT MODE
+            # アイコンの選択状態
             toggle_dark_light.selected = not toggle_dark_light.selected
+            # ToolTipの表示文言更新
             toggle_dark_light.tooltip = f"switch light and dark mode (currently {'dark' if toggle_dark_light.selected else 'light'} mode)"
             # if toggle_dark_light.selected:
             #     page.appbar.title = Text(
-            #         f"MyApp", size=32, color=ft.colors.BLACK45)
-            #     page.appbar.leading = IconButton(
-            #         icons.MENU, icon_color=ft.colors.BLACK45)
+            #         f"MyApp", size=32, color=ft.colors.YELLOW_500)
             # else:
             #     page.appbar.title = Text(
-            #         f"MyApp", size=32, color=ft.colors.BLACK)
-            #     page.appbar.leading = IconButton(
-            #         icons.MENU, icon_color=ft.colors.BLACK)
+            #         f"MyApp", size=32, color=ft.colors.WHITE)
 
-            # AND DISABLE AGAIN THE PROGRESSBAR WHEN CHANGE DARK MODE
+            # プログレスバー非表示
             page.splash.visible = False
             page.update()
 
-        # CREATE TOGLE BUTTON DARK MODE LIGHT
+        # 切り替え用のボタン
         toggle_dark_light = IconButton(
+            # ToolTipの表示文言
             tooltip=f"switch light and dark mode (currently light mode)",
             on_click=change_theme,
             icon="light_mode",
