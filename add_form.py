87579af3d0f65ee4d8f93fc9e05e0449336d9335
eno-conan <eno49study@gmail.com>
2023-07-10 15:23:31 +0900

    日付のバリデーションチェック、エラーメッセージ表示実装

--- add_form.py
+++ add_form.py
@@ -1,6 +1,5 @@
 import flet as ft
 from flet import (
-    AppBar,
     Card,
     Column,
     Container,
@@ -12,7 +11,7 @@ from flet import (
     Text,
     icons
 )
-import re
+import datetime
 
 
 class TextFieldsAndSubmit():
@@ -32,18 +31,18 @@ class TextFieldsAndSubmit():
                 # create POST request
                 # url = os.getenv('WORKERS_URL')
                 # data = {
-                #     'CompanyName': company_name_textfield.value,
-                #     'ContactName': contract_name_textfield.value,
+                #     'CompanyName': seminar_name_textfield.value,
+                #     'ContactName': description_textfield.value,
                 # }
                 # data_encode = json.dumps(data)
                 # requests.post(url, data=data_encode)
 
-                # clear input values
-                company_name_textfield.value = ""
-                contract_name_textfield.value = ""
+                # 入力値初期化
+                seminar_name_textfield.value = ""
+                description_textfield.value = ""
                 category_text.value = None
                 date_textfield.value = ""
-                # close Modal
+                # Modal
                 page.dialog.open = False
                 page.banner.open = True
                 # page.snack_bar.open = True
@@ -54,24 +53,49 @@ class TextFieldsAndSubmit():
                 # msg_failed_add_customer.visible = True
                 # page.update()
 
+        # 不正日付のメッセージ表示
+        msg_invalid_date_text = ft.Text()
+        msg_invalid_date = Row(
+            [
+                ft.Text("日付の形式がyyyy/mm/ddではない、または存在しない日付です。",
+                        size=20, color=ft.colors.RED_500
+                        )
+            ],
+            visible=False
+        )
+
         def textfield_change(e):
-            if company_name_textfield.value == "" or contract_name_textfield.value == "":
+            msg_invalid_date.visible = False
+            if seminar_name_textfield.value == "" or description_textfield.value == "":
                 submit_button.disabled = True
             elif len(date_textfield.value) != 10:
                 submit_button.disabled = True
             else:
-                date_pattern = "^20[0-9]{2}/(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])$"
-                res = re.match(date_pattern, date_textfield.value)
-                if res is not None:
-                    print(date_textfield.value)
-                    submit_button.disabled = False
+                is_exist_date = False
+                try:
+                    date_split_slush = date_textfield.value.split('/')
+                    if len(date_split_slush) != 3:
+                        # スラッシュ分割で、配列の大きさが3ではない場合、フォーマット不正
+                        # msg_invalid_date_text.value = '日付の入力形式が正しくないです'
+                        msg_invalid_date.visible = True
+                    else:
+                        # 存在日付チェック
+                        new_data_str = "%04s/%02s/%02s" % (
+                            date_split_slush[0], date_split_slush[1], date_split_slush[2])
+                        datetime.datetime.strptime(new_data_str, "%Y/%m/%d")
+                        is_exist_date = True
+                        if is_exist_date:
+                            submit_button.disabled = False
+                except:
+                    # msg_invalid_date_text.value = '日付の入力形式が正しくないです'
+                    msg_invalid_date.visible = True
             page.update()
 
         # text fields
-        company_name_textfield = ft.TextField(
-            label="Company Name", on_change=textfield_change)
-        contract_name_textfield = ft.TextField(
-            label="Contract Name", on_change=textfield_change)
+        seminar_name_textfield = ft.TextField(
+            label="Seminar Name", on_change=textfield_change)
+        description_textfield = ft.TextField(
+            label="Description", on_change=textfield_change)
 
         def dropdown_changed(e):
             category_text.value = dd.value
@@ -79,19 +103,17 @@ class TextFieldsAndSubmit():
 
         # DropDown(カテゴリ)
         category_text = ft.Text()
+        categories_arr = []
+        for category in ['Java', 'Python', 'Flutter', 'React', 'Javascript', 'Docker']:
+            categories_arr.append(ft.dropdown.Option(category))
         dd = ft.Dropdown(
             on_change=dropdown_changed,
             bgcolor=ft.colors.BLUE_200,
-            options=[
-                ft.dropdown.Option("Java"),
-                ft.dropdown.Option("Python"),
-                ft.dropdown.Option("Flutter"),
-                ft.dropdown.Option("React"),
-                ft.dropdown.Option("Javascript"),
-            ],
+            options=categories_arr,
             width=300,
         )
 
+        # 開催日（テキストフィールド）
         date_textfield = ft.TextField(
             width=300,
             label="Please Input yyyy/mm/dd",
@@ -117,21 +139,17 @@ class TextFieldsAndSubmit():
             ],
         )
 
-        # このモーダルの内容が初期表示段階で構築されるから、各値が何も表示できていない。
-        # 表示内容を都度更新できるように工夫：モーダルを開くタイミングでダイアログを構築する
-        # https://flet.dev/docs/controls/alertdialog
         def open_dlg_modal(e):
             page.dialog = ft.AlertDialog(
                 modal=True,
                 title=ft.Text("Confirm Create Seminar"),
-                # content=ft.Text("Do you really want to delete all those files?"),
                 content=ft.Container(
                     height=150,
                     width=200,
                     content=Column(
                         [
-                            ft.Text(company_name_textfield.value),
-                            ft.Text(contract_name_textfield.value),
+                            ft.Text(seminar_name_textfield.value),
+                            ft.Text(description_textfield.value),
                             ft.Text(category_text.value),
                             ft.Text(date_textfield.value),
                         ]
@@ -149,12 +167,13 @@ class TextFieldsAndSubmit():
         def close_dlg(e):
             page.dialog.open = False
             page.update()
-        
+
         # Submit
         submit_button = ft.ElevatedButton(
             disabled=True,
             text="Submit", on_click=open_dlg_modal)
 
+        # 表示内容
         contents.controls.append(
             Column(
                 [
@@ -162,7 +181,7 @@ class TextFieldsAndSubmit():
                               margin=ft.margin.symmetric(horizontal=20,
                               vertical=10)),
                     Container(content=Column(
-                        [company_name_textfield, contract_name_textfield]
+                        [seminar_name_textfield, description_textfield]
                     ),
                         margin=ft.margin.symmetric(horizontal=30, vertical=10)
                     ),
@@ -177,8 +196,12 @@ class TextFieldsAndSubmit():
                               margin=ft.margin.symmetric(horizontal=20,)
                               ),
                     Container(content=date_textfield,
+                              margin=ft.margin.only(
+                                  left=30, top=10)
+                              ),
+                    Container(content=msg_invalid_date,
                               margin=ft.margin.symmetric(
-                                  horizontal=30, vertical=10)
+                                  horizontal=30)
                               ),
                     Container(content=submit_button,
                               margin=ft.margin.only(right=25),
